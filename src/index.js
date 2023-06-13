1487e6f66ac4be2382f455a86640a73ea687635d
Alexander Belov <marlo-nn@yandex.ru>
2023-06-13 21:26:57 +0300

    заменил на хэш роутер

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 import './pages/index.css';
 import App from './components/App';
@@ -8,9 +8,9 @@ import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 
 
 );
